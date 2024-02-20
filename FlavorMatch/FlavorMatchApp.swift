e909074bc0490fdebb9f14f324eb29d617cd6058
shardakova svetlana <shardakova.s@medlinx.online>
2024-02-20 11:34:53 +0100

    Initial Commit

---
+++ FlavorMatch/FlavorMatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlavorMatchApp.swift
+//  FlavorMatch
+//
+//  Created by Svetlana Shardakova on 20/02/24.
+//
+
+import SwiftUI
+
+@main
+struct FlavorMatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
